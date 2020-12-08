10416f532cc3fcffdbf611eb4668da2c4e492a1b
phorvicheka <phorvicheka@yahoo.com>
2020-12-08 20:35:27 +0900

    Speech translator

--- samples/java/android/sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/speechtranslator/MainActivity.java
+++ samples/java/android/sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/speechtranslator/MainActivity.java
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
 //
-package com.microsoft.cognitiveservices.speech.samples.sdkdemo;
+package com.microsoft.cognitiveservices.speech.samples.speechtranslator;
 
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
--- samples/java/android/sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/speechtranslator/SpeechTranslationActivity.java
+++ samples/java/android/sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/speechtranslator/SpeechTranslationActivity.java
@@ -1,4 +1,4 @@
-package com.microsoft.cognitiveservices.speech.samples.sdkdemo;
+package com.microsoft.cognitiveservices.speech.samples.speechtranslator;
 
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;

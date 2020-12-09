b9b2eba33a4b37086b44588068bd2449934e10aa
phorvicheka <phorvicheka@yahoo.com>
2020-12-09 13:33:40 +0900

    Replace TextView with EditText
    https://stackoverflow.com/questions/1555109/how-to-stop-edittext-from-gaining-focus-at-activity-startup-in-android
    Adding the tag android:focusableInTouchMode="true" and android:focusable="true" to parent layout
    
    https://gist.github.com/sc0rch/7c982999e5821e6338c25390f50d2993
    Clear focus on touch outside for all EditText inputs.

--- samples/java/android/sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/speechtranslator8TTS/SpeechTranslationActivity.java
+++ samples/java/android/sdkdemo/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/speechtranslator8TTS/SpeechTranslationActivity.java
@@ -1,15 +1,20 @@
 package com.microsoft.cognitiveservices.speech.samples.speechtranslator8TTS;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -69,8 +74,8 @@ public class SpeechTranslationActivity extends AppCompatActivity implements Adap
     private final File myDirectory = new File(Environment.getExternalStorageDirectory(), "KK-sdkDemoSpeechTranslator");
     private Map<String, String> languageToVoiceMap;
 
-    private TextView recognizedTextView;
-    private TextView translatedTextView;
+    private EditText recognizedTextView;
+    private EditText translatedTextView;
 
     private Button recognizeButton;
     private Button recognizeIntermediateButton;
@@ -522,6 +527,27 @@ public class SpeechTranslationActivity extends AppCompatActivity implements Adap
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    /**
+     * Clear focus on touch outside for all EditText inputs.
+     * https://gist.github.com/sc0rch/7c982999e5821e6338c25390f50d2993
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 
 }
